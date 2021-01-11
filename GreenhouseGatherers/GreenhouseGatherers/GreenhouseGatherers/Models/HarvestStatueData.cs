174e8f070e42ca1cebe50ab40f42539b382994fa
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-01-10 21:46:07 -0600

    Created HarvestStatueData for caching via SaveData

---
+++ GreenhouseGatherers/GreenhouseGatherers/GreenhouseGatherers/Models/HarvestStatueData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenhouseGatherers.GreenhouseGatherers.Models
+{
+    public class HarvestStatueData
+    {
+        public string GameLocation { get; set; }
+        public Vector2 Tile { get; set; }
+
+        public HarvestStatueData(string gameLocation, Vector2 tile)
+        {
+            this.GameLocation = gameLocation;
+            this.Tile = tile;
+        }
+    }
+}
