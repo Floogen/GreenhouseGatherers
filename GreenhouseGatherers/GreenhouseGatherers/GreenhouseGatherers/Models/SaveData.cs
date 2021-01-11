d77290d8b2d75b435775c8cc7e1c6720bcaca3c5
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-01-10 21:45:50 -0600

    Created SaveData for caching via JSON

---
+++ GreenhouseGatherers/GreenhouseGatherers/GreenhouseGatherers/Models/SaveData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenhouseGatherers.GreenhouseGatherers.Models
+{
+    public class SaveData
+    {
+        public List<HarvestStatueData> SavedStatueData { get; set; }
+
+        public SaveData()
+        {
+            SavedStatueData = new List<HarvestStatueData>();
+        }
+
+        public SaveData(List<HarvestStatueData> savedStatueData)
+        {
+            SavedStatueData = savedStatueData;
+        }
+    }
+}
