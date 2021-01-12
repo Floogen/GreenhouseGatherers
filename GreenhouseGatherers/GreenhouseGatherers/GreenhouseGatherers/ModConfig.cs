da7327f60e9d5745017ce6d6625c0c93239dafee
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-01-12 17:34:32 -0600

    Created EnableHarvestMessage option

--- GreenhouseGatherers/GreenhouseGatherers/GreenhouseGatherers/ModConfig.cs
+++ GreenhouseGatherers/GreenhouseGatherers/GreenhouseGatherers/ModConfig.cs
@@ -14,6 +14,7 @@ namespace GreenhouseGatherers.GreenhouseGatherers
         public bool DoJunimosAppearAfterHarvest { get; set; }
         public bool DoJunimosHarvestFromFruitTrees { get; set; }
         public bool ForceRecipeUnlock { get; set; }
+        public bool EnableHarvestMessage { get; set; }
 
         public int MaxAmountOfJunimosToAppearAfterHarvest { get; set; }
         public int MinimumFruitOnTreeBeforeHarvest { get; set; }
@@ -24,7 +25,9 @@ namespace GreenhouseGatherers.GreenhouseGatherers
             DoJunimosHarvestFromPots = true;
             DoJunimosAppearAfterHarvest = true;
             DoJunimosHarvestFromFruitTrees = true;
+
             ForceRecipeUnlock = false;
+            EnableHarvestMessage = true;
 
             MaxAmountOfJunimosToAppearAfterHarvest = -1;
             MinimumFruitOnTreeBeforeHarvest = 3;
