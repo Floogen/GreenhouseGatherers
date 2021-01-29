383d7406096ed55147bfd9fa93d7e0fcf1648249
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-01-29 14:16:08 -0600

    Added static resources class

---
+++ GreenhouseGatherers/GreenhouseGatherersAutomate/GreenhouseGatherersAutomate/AutomateModResources.cs
@@ -0,0 +1,24 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenhouseGatherersAutomate.GreenhouseGatherersAutomate
+{
+    public static class AutomateModResources
+    {
+		private static IMonitor monitor;
+
+		public static void LoadMonitor(IMonitor iMonitor)
+		{
+			monitor = iMonitor;
+		}
+
+		public static IMonitor GetMonitor()
+		{
+			return monitor;
+		}
+	}
+}
