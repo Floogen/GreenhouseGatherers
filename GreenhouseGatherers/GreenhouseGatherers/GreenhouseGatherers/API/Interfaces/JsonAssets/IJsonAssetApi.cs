2dfc22a06fe2000a7f82dd3411fa7d62d2cb48a5
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-03-03 16:13:17 -0600

    Added LoadAssets to Json Asset's API

--- GreenhouseGatherers/GreenhouseGatherers/GreenhouseGatherers/API/Interfaces/JsonAssets/IJsonAssetApi.cs
+++ GreenhouseGatherers/GreenhouseGatherers/GreenhouseGatherers/API/Interfaces/JsonAssets/IJsonAssetApi.cs
@@ -5,6 +5,7 @@ namespace GreenhouseGatherers.GreenhouseGatherers.API.Interfaces.JsonAssets
 {
     public interface IJsonAssetApi
     {
+        void LoadAssets(string path);
         List<string> GetAllBigCraftablesFromContentPack(string cp);
         IDictionary<string, int> GetAllBigCraftableIds();
         int GetBigCraftableId(string name);
